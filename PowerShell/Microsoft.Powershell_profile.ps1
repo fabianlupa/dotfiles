751db502bc0a4d9c492eea82db1160bfa5420cd8
Fabian Lupa <fabian@lupa.dev>
2024-01-15 18:54:40 +0100

    Update

--- PowerShell/Microsoft.Powershell_profile.ps1
+++ PowerShell/Microsoft.Powershell_profile.ps1
@@ -1,3 +1,9 @@
 Import-Module LightSwitch
 
 Set-Alias st Switch-Theme
+
+# Invoke-Expression (&starship init powershell)
+
+oh-my-posh init pwsh | Invoke-Expression
+
+$env:POSH_GIT_ENABLED = $true
