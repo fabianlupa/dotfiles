3da418c26687066992c36ea3a09740253e0d1b35
Fabian Lupa <fabian@lupa.dev>
2024-09-15 09:16:08 +0200

    Add comments and cleanup powershell settings

--- PowerShell/Microsoft.Powershell_profile.ps1
+++ PowerShell/Microsoft.Powershell_profile.ps1
@@ -10,16 +10,17 @@ Set-Alias st Switch-Theme
 Set-Alias mountp Mount-P
 Set-Alias unmountp Remove-MountP
 
+# Disable italics in autocomplete
 set-psreadlineoption -Colors @{ "InlinePrediction" = "`e[38;5;238m" }
+
+# Disable italics in list headers
 $PSStyle.Formatting.CustomTableHeaderLabel = $PSStyle.Formatting.TableHeader
 
-# Invoke-Expression (&starship init powershell)
-
-# oh-my-posh init pwsh | Invoke-Expression
-
+# Wrap oh-my-posh init for asynchronous load
 function prompt {
     $env:POSH_THEME = "~/AppData/Local/Programs/oh-my-posh/themes/agnoster.minimal.omp.json" 
     oh-my-posh init pwsh | Invoke-Expression
 }
+# oh-my-posh init pwsh | Invoke-Expression
 
 $env:POSH_GIT_ENABLED = $true
