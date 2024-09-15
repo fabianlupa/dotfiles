55d8289066eccdf07aeb5d684b32c9f83315f93b
Fabian Lupa <fabian@lupa.dev>
2024-09-15 08:47:24 +0200

    Sync device settings

--- PowerShell/Microsoft.Powershell_profile.ps1
+++ PowerShell/Microsoft.Powershell_profile.ps1
@@ -10,8 +10,15 @@ Set-Alias st Switch-Theme
 Set-Alias mountp Mount-P
 Set-Alias unmountp Remove-MountP
 
+set-psreadlineoption -Colors @{ "InlinePrediction" = "`e[38;5;238m" }
+
 # Invoke-Expression (&starship init powershell)
 
-oh-my-posh init pwsh | Invoke-Expression
+# oh-my-posh init pwsh | Invoke-Expression
+
+function prompt {
+    $env:POSH_THEME = "~/AppData/Local/Programs/oh-my-posh/themes/agnoster.minimal.omp.json" 
+    oh-my-posh init pwsh | Invoke-Expression
+}
 
 $env:POSH_GIT_ENABLED = $true
